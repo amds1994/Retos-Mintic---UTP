adb493780d882e7d6e673768cddca585d77bb4fd
Anmaudo <61362589+amds1994@users.noreply.github.com>
2022-08-13 15:59:01 -0500

    Add files via upload

---
+++ Reto5/src/main/java/com/mycompany/reto5/controlador/ControladorMain.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto5.controlador;
+
+import com.mycompany.reto5.vista.frmnPrincipal;
+
+
+
+/**
+ *
+ * @author Andres Dominguez
+ */
+public class ControladorMain {
+    public static void main(String[] args) {
+        new frmnPrincipal().setVisible(true);
+    }
+}
